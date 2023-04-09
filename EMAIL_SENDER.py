1f20b0f66504cb72d1d9e50aaee65cf6c8eacfae
Abhinav Varshney <115035256+AbhinavBoss@users.noreply.github.com>
2023-04-09 16:25:27 +0530

    Update EMAIL_SENDER.py

--- EMAIL_SENDER.py
+++ EMAIL_SENDER.py
@@ -2,11 +2,11 @@ from email.message import EmailMessage
 from password_forEmail import password
 import ssl
 import smtplib
-email_sender = 'varshneyabhinav66@gmail.com'
+email_sender = "SENDER's EMAIL"
 email_password = password
-email_reciever = '221b013@juetguna.in'
-subject = 'Practicing python email module'
-body = "I'm Abhinav Varshney and I want to become a pro programmer and fantastic developer."
+email_reciever = "RECEIVER's EMAIL"
+subject = 'WRITE A SUBJECT'
+body = "WRITE THE MAIN CONTENT OF THE MAIL HERE"
 em = EmailMessage()
 em['From'] = email_sender
 em['To'] = email_reciever
@@ -14,6 +14,6 @@ em['subject'] = subject
 em.set_content(body)
 context = ssl.create_default_context()
 with smtplib.SMTP_SSL('smtp.gmail.com',465,context=context) as smtp:
-    print("EMAIL SEND HO GYA HAI")
+    print("EMAIL SEND HO GYA HAI")  #YOU CAN WRITE ANY MESSAGE HERE
     smtp.login(email_sender,email_password)
     smtp.sendmail(email_sender,email_reciever,em.as_string())

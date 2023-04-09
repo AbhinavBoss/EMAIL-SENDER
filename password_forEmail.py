225f4a6e0d3e9e3cffff35cbbf2b824b478ca926
Abhinav Varshney <115035256+AbhinavBoss@users.noreply.github.com>
2023-04-09 16:30:25 +0530

    Update password_forEmail.py

--- password_forEmail.py
+++ password_forEmail.py
@@ -1,1 +1,2 @@
+#you can directly write your password in the main program but for security concerns you can hide the password file somewhere and use it like this
 password = 'PASTE THE CODE YOU COPIED FROM THE ACCOUNT'
